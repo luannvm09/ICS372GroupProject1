c7630db514f007463cce2f4d967737143a29c981
Jack Henry <jack@jackerickson.net>
2021-02-22 12:14:27 -0600

    Initital Commit. Created repository and .gitignore file.

---
+++ src/Driver.java
@@ -0,0 +1,13 @@
+/**
+ * Driver class for running application. Contains main function.
+ */
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Write Code
+		System.out.println("Running...");
+		System.out.println("Done.");
+		return;
+	}
+
+}
