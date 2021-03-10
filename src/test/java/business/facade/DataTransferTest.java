2276476d82b9e617cd764f74a8ff762f9ff748ea
Jack Henry <jack@jackerickson.net>
2021-03-10 15:09:27 -0600

    Generated unit tests for DataTransfer.java.

---
+++ src/test/java/business/facade/DataTransferTest.java
@@ -0,0 +1,88 @@
+package business.facade;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataTransferTest {
+
+    @Test
+    public void getMemberId() {
+    }
+
+    @Test
+    public void setMemberId() {
+    }
+
+    @Test
+    public void getMemberName() {
+    }
+
+    @Test
+    public void setMemberName() {
+    }
+
+    @Test
+    public void getMemberAddress() {
+    }
+
+    @Test
+    public void setMemberAddress() {
+    }
+
+    @Test
+    public void getMemberPhoneNumber() {
+    }
+
+    @Test
+    public void setMemberPhoneNumber() {
+    }
+
+    @Test
+    public void getDateJoined() {
+    }
+
+    @Test
+    public void setDateJoined() {
+    }
+
+    @Test
+    public void getFeePaid() {
+    }
+
+    @Test
+    public void setFeePaid() {
+    }
+
+    @Test
+    public void getProductId() {
+    }
+
+    @Test
+    public void setProductId() {
+    }
+
+    @Test
+    public void getProductName() {
+    }
+
+    @Test
+    public void setProductName() {
+    }
+
+    @Test
+    public void getStockOnHand() {
+    }
+
+    @Test
+    public void setStockOnHand() {
+    }
+
+    @Test
+    public void getCurrentPrice() {
+    }
+
+    @Test
+    public void setCurrentPrice() {
+    }
+}
