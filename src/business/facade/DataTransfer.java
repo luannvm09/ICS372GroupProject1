f2a66008854b859473e6ce367a4f5b29539471ae
Jack Henry <jack@jackerickson.net>
2021-03-10 13:17:48 -0600

    Refactored Product.java and DataTransfer.java to conform to style guidelines.

--- src/business/facade/DataTransfer.java
+++ src/business/facade/DataTransfer.java
@@ -4,15 +4,17 @@ import business.entities.Product;
 
 /**
  * DataTransfer is a class that will carry out the transfer of data from the UI
- * and the GroceryStore class.  It will store the instance variables of both member
- * and product.  These fields can be supplied and sent in a request, or used to 
- * return values in a result.
+ * and the GroceryStore class. It will store the instance variables of both
+ * member and product. These fields can be supplied and sent in a request, or
+ * used to return values in a result.
  * 
  * @author jordan dodd
  */
-public abstract class DataTransfer {  // FIXME this class does not contian a setMemberFields() method yet
+public abstract class DataTransfer {
+	// FIXME this class does not contian a setMemberFields() method yet
 	private final static int NONE_INT = Integer.MIN_VALUE;
 	private final static double NONE_DOUBLE = Double.MIN_VALUE;
+	private final static String NONE_STRING = "none";
 	// Member fields
 	private String memberId;
 	private String memberName;
@@ -23,22 +25,22 @@ public abstract class DataTransfer {  // FIXME this class does not contian a set
 	// Product fields
 	private String productId;
 	private String productName;
-	private int stockOnHand;
 	private double currentPrice;
+	private int stockOnHand;
 	private int reorderLevel;
-	
+
 	/**
 	 * constructor sets all fields to none
 	 */
 	public DataTransfer() {
 		reset();
 	}
-	
+
 	/**
 	 * Getters and setters
 	 */
 	public String getMemberId() {
-		return memberId;
+		return this.memberId;
 	}
 
 	public void setMemberId(String memberId) {
@@ -46,7 +48,7 @@ public abstract class DataTransfer {  // FIXME this class does not contian a set
 	}
 
 	public String getMemberName() {
-		return memberName;
+		return this.memberName;
 	}
 
 	public void setMemberName(String memberName) {
@@ -54,7 +56,7 @@ public abstract class DataTransfer {  // FIXME this class does not contian a set
 	}
 
 	public String getMemberAddress() {
-		return memberAddress;
+		return this.memberAddress;
 	}
 
 	public void setMemberAddress(String memberAddress) {
@@ -62,7 +64,7 @@ public abstract class DataTransfer {  // FIXME this class does not contian a set
 	}
 
 	public String getMemberPhoneNumber() {
-		return memberPhoneNumber;
+		return this.memberPhoneNumber;
 	}
 
 	public void setMemberPhoneNumber(String memberPhoneNumber) {
@@ -70,7 +72,7 @@ public abstract class DataTransfer {  // FIXME this class does not contian a set
 	}
 
 	public String getDateJoined() {
-		return dateJoined;
+		return this.dateJoined;
 	}
 
 	public void setDateJoined(String dateJoined) {
@@ -78,7 +80,7 @@ public abstract class DataTransfer {  // FIXME this class does not contian a set
 	}
 
 	public String getFeePaid() {
-		return feePaid;
+		return this.feePaid;
 	}
 
 	public void setFeePaid(String feePaid) {
@@ -86,7 +88,7 @@ public abstract class DataTransfer {  // FIXME this class does not contian a set
 	}
 
 	public String getProductId() {
-		return productId;
+		return this.productId;
 	}
 
 	public void setProductId(String productId) {
@@ -94,17 +96,15 @@ public abstract class DataTransfer {  // FIXME this class does not contian a set
 	}
 
 	public String getProductName() {
-		return productName;
+		return this.productName;
 	}
 
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
 
-
-
 	public int getStockOnHand() {
-		return stockOnHand;
+		return this.stockOnHand;
 	}
 
 	public void setStockOnHand(int stockOnHand) {
@@ -112,7 +112,7 @@ public abstract class DataTransfer {  // FIXME this class does not contian a set
 	}
 
 	public double getCurrentPrice() {
-		return currentPrice;
+		return this.currentPrice;
 	}
 
 	public void setCurrentPrice(double currentPrice) {
@@ -120,49 +120,51 @@ public abstract class DataTransfer {  // FIXME this class does not contian a set
 	}
 
 	public int getReorderLevel() {
-		return reorderLevel;
+		return this.reorderLevel;
 	}
 
 	public void setReorderLevel(int reorderLevel) {
 		this.reorderLevel = reorderLevel;
 	}
-	
+
 	/**
-	 * This class will set the product fields with the values from a passed
-	 * in product.  It will leave the member fields in this DataTransfer = "none"
+	 * This class will set the product fields with the values from a passed in
+	 * product. It will leave the member fields in this DataTransfer =
+	 * DataTransfer.NONE_STRING
+	 * 
 	 * @param product
 	 */
 	public void setProductFields(Product product) {
-		productId = product.getProductId();
-		productName = product.getProductName();
-		stockOnHand = product.getStockOnHand();
-		currentPrice = product.getCurrentPrice();
-		reorderLevel = product.getReorderLevel();
+		this.productId = product.getProductId();
+		this.productName = product.getProductName();
+		this.stockOnHand = product.getStockOnHand();
+		this.currentPrice = product.getCurrentPrice();
+		this.reorderLevel = product.getReorderLevel();
 	}
+
 	/**
-	 *  Reset sets all fields to a default value of NONE
-	 *  or constants NONE_INT and NONE_DOUBLE.  reset() does not
-	 *  return null values, so it is clear when a field does not contain
-	 *  meaningful data.  Constants are used as to not confuse a sentinel
-	 *  value for an actual requested value.
-	 *  @param none
-	 *  @return void
+	 * Reset sets all fields to a default value of NONE or constants NONE_INT and
+	 * NONE_DOUBLE. reset() does not return null values, so it is clear when a field
+	 * does not contain meaningful data. Constants are used as to not confuse a
+	 * sentinel value for an actual requested value.
+	 * 
+	 * @param none
+	 * @return void
 	 */
 	public void reset() {
-		//member fields
-		memberId = "none";
-		memberName = "none";
-		memberAddress = "none";
-		memberPhoneNumber = "none";
-		dateJoined = "none";
-		feePaid = "none";
-		//product fields
-		productId = "none";
-		productName = "none";
-		stockOnHand = NONE_INT;
-		currentPrice = NONE_DOUBLE;
-		reorderLevel = NONE_INT;
-		
+		// member fields
+		memberId = DataTransfer.NONE_STRING;
+		memberName = DataTransfer.NONE_STRING;
+		memberAddress = DataTransfer.NONE_STRING;
+		memberPhoneNumber = DataTransfer.NONE_STRING;
+		dateJoined = DataTransfer.NONE_STRING;
+		feePaid = DataTransfer.NONE_STRING;
+		// product fields
+		productId = DataTransfer.NONE_STRING;
+		productName = DataTransfer.NONE_STRING;
+		stockOnHand = DataTransfer.NONE_INT;
+		currentPrice = DataTransfer.NONE_DOUBLE;
+		reorderLevel = DataTransfer.NONE_INT;
 	}
 
 }
