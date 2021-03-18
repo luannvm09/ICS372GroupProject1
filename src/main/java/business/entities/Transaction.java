c92144ccd23994ce7916ce27169c92eab9b7e2fd
Aisha Ahmed <79179996+aahm2221@users.noreply.github.com>
2021-03-18 16:12:07 -0500

    Print transactions function added

--- src/main/java/business/entities/Transaction.java
+++ src/main/java/business/entities/Transaction.java
@@ -2,73 +2,85 @@ package business.entities;
 
 import java.io.Serializable;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 public class Transaction implements Serializable {
-    private static final long serialVersionUID = 1L;
-    private String type;
-    private String name;
-    private Calendar date;
+	private static final long serialVersionUID = 1L;
+	private String type;
+	private String name;
+	private Calendar date;
 
-    /**
-     * Creates the transaction with a given type and product name. The date is the
-     * current date.
-     * 
-     * @param type The type of transaction
-     * @param name The name of product
-     * 
-     */
-    public Transaction(String type, String name) {
-        this.type = type;
-        this.name = name;
-        date = new GregorianCalendar();
-    }
+	/**
+	 * Creates the transaction with a given type and product name. The date is the
+	 * current date.
+	 * 
+	 * @param type The type of transaction
+	 * @param name The name of product
+	 * 
+	 */
+	public Transaction(String type, String name) {
+		this.type = type;
+		this.name = name;
 
-    /**
-     * Checks whether this transaction is on the given date
-     * 
-     * @param date The date for which transactions are being sought
-     * @return true iff the dates match
-     */
-    public boolean onDate(Calendar date) {
-        return ((date.get(Calendar.YEAR) == this.date.get(Calendar.YEAR))
-                && (date.get(Calendar.MONTH) == this.date.get(Calendar.MONTH))
-                && (date.get(Calendar.DATE) == this.date.get(Calendar.DATE)));
-    }
+	}
 
-    /**
-     * Returns the type field
-     * 
-     * @return type field
-     */
-    public String getType() {
-        return type;
-    }
+	/**
+	 * Checks whether this transaction is on the given date
+	 * 
+	 * @param date The date for which transactions are being sought
+	 * @return true iff the dates match
+	 */
+	public boolean onDate(Calendar date) {
+		return ((date.get(Calendar.YEAR) == this.date.get(Calendar.YEAR))
+				&& (date.get(Calendar.MONTH) == this.date.get(Calendar.MONTH))
+				&& (date.get(Calendar.DATE) == this.date.get(Calendar.DATE)));
+	}
 
-    /**
-     * Returns the name field
-     * 
-     * @return name field
-     */
-    public String getName() {
-        return name;
-    }
+	/**
+	 * Checks if transaction was between two dates specified
+	 * 
+	 * @param startDate start date of period to check
+	 * @param endDate   end date of period to check
+	 * @return true if the transaction was between the two dates
+	 */
+	public boolean betweenDates(Calendar startDate, Calendar endDate) {
 
-    /**
-     * Returns the date as a String
-     * 
-     * @return date with month, date, and year
-     */
-    public String getDate() {
-        return date.get(Calendar.MONTH) + "/" + date.get(Calendar.DATE) + "/" + date.get(Calendar.YEAR);
-    }
+		return ((this.date.after(startDate) && this.date.before(endDate)) || this.date.equals(startDate)
+				|| this.date.equals(endDate));
+	}
 
-    /**
-     * String form of the transaction
-     * 
-     */
-    @Override
-    public String toString() {
-        return (type + "   " + name);
-    }
+	/**
+	 * Returns the type field
+	 * 
+	 * @return type field
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * Returns the name field
+	 * 
+	 * @return name field
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Returns the date as a String
+	 * 
+	 * @return date with month, date, and year
+	 */
+	public String getDate() {
+		return date.get(Calendar.MONTH) + "/" + date.get(Calendar.DATE) + "/" + date.get(Calendar.YEAR);
+	}
+
+	/**
+	 * String form of the transaction
+	 * 
+	 */
+	@Override
+	public String toString() {
+		return (type + "   " + name);
+	}
 }
--- src/main/java/business/facade/Grocery.java
+++ src/main/java/business/facade/Grocery.java
@@ -5,188 +5,206 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-import business.entities.Product;
-import business.entities.iterator.ReadOnlyIterator;
 import business.entities.Member;
+import business.entities.Product;
+import business.entities.Transaction;
+import business.entities.iterator.ReadOnlyIterator;
 
 public class Grocery implements Serializable {
-    private static final long serialVersionUID = 1L;
-    private Stock stock = new Stock();
-    private MemberList members = new MemberList();
-    private static Grocery grocery;
+	private static final long serialVersionUID = 1L;
+	private Stock stock = new Stock();
+	private MemberList members = new MemberList();
+	private static Grocery grocery;
 
-    private class Stock implements Iterable<Product>, Serializable {
-        private static final long serialVersionUID = 1L;
-        private List<Product> products = new LinkedList<Product>();
+	private class Stock implements Iterable<Product>, Serializable {
+		private static final long serialVersionUID = 1L;
+		private List<Product> products = new LinkedList<Product>();
 
-        /**
-         * Checks whether a product with a given product id exists.
-         * 
-         * @param productId the id of the product
-         * @return true iff the book exists
-         * 
-         */
-        public Product search(String productId) {
-            for (Iterator<Product> iterator = products.iterator(); iterator.hasNext();) {
-                Product product = (Product) iterator.next();
-                if (product.getProductId().equals(productId)) {
-                    return product;
-                }
-            }
-            return null;
-        }
+		/**
+		 * Checks whether a product with a given product id exists.
+		 * 
+		 * @param productId the id of the product
+		 * @return true iff the book exists
+		 * 
+		 */
+		public Product search(String productId) {
+			for (Iterator<Product> iterator = products.iterator(); iterator.hasNext();) {
+				Product product = (Product) iterator.next();
+				if (product.getProductId().equals(productId)) {
+					return product;
+				}
+			}
+			return null;
+		}
 
-        /**
-         * Removes a product from the stock
-         * 
-         * @param productId product id
-         * @return true if product could be removed
-         */
-        public boolean removeProduct(String productId) {
-            Product product = search(productId);
-            if (product == null) {
-                return false;
-            } else {
-                return products.remove(product);
-            }
-        }
+		/**
+		 * Removes a product from the stock
+		 * 
+		 * @param productId product id
+		 * @return true if product could be removed
+		 */
+		public boolean removeProduct(String productId) {
+			Product product = search(productId);
+			if (product == null) {
+				return false;
+			} else {
+				return products.remove(product);
+			}
+		}
 
-        /**
-         * Adding a product into the stock
-         * 
-         * @param product the product to be added
-         * @return true iff the product could be added. Currently always true
-         */
-        public boolean addProduct(Product product) {
-            products.add(product);
-            return true;
-        }
+		/**
+		 * Adding a product into the stock
+		 * 
+		 * @param product the product to be added
+		 * @return true iff the product could be added. Currently always true
+		 */
+		public boolean addProduct(Product product) {
+			products.add(product);
+			return true;
+		}
 
-        /**
-         * Returns an iterator to all products
-         * 
-         * @return iterator to the stock
-         */
-        public Iterator<Product> iterator() {
-            return products.iterator();
-        }
+		/**
+		 * Returns an iterator to all products
+		 * 
+		 * @return iterator to the stock
+		 */
+		public Iterator<Product> iterator() {
+			return products.iterator();
+		}
 
-        public Iterator<Product> safeIterator() {
-            return new ReadOnlyIterator<Product>(products);
-        }
+		public Iterator<Product> safeIterator() {
+			return new ReadOnlyIterator<Product>(products);
+		}
 
-        /**
-         * String form of the stock collection
-         * 
-         */
-        public String toString() {
-            return products.toString();
-        }
-    }
+		/**
+		 * String form of the stock collection
+		 * 
+		 */
+		public String toString() {
+			return products.toString();
+		}
+	}
 
-    private class MemberList implements Iterable<Member>, Serializable {
-        private static final long serialVersionUID = 1L;
-        private List<Member> members = new LinkedList<Member>();
+	private class MemberList implements Iterable<Member>, Serializable {
+		private static final long serialVersionUID = 1L;
+		private List<Member> members = new LinkedList<Member>();
 
-        /**
-         * Checks whether a member with a given member id exists.
-         * 
-         * @param memberId the id of the member
-         * @return true iff member exists
-         * 
-         */
-        public Member search(String memberId) {
-            for (Iterator<Member> iterator = members.iterator(); iterator.hasNext();) {
-                Member member = iterator.next();
-                if (member.getMemberId().equals(memberId)) {
-                    return member;
-                }
-            }
-            return null;
-        }
+		/**
+		 * Checks whether a member with a given member id exists.
+		 * 
+		 * @param memberId the id of the member
+		 * @return true iff member exists
+		 * 
+		 */
+		public Member search(String memberId) {
+			for (Iterator<Member> iterator = members.iterator(); iterator.hasNext();) {
+				Member member = iterator.next();
+				if (member.getMemberId().equals(memberId)) {
+					return member;
+				}
+			}
+			return null;
+		}
 
-        /**
-         * Inserts a member into the collection
-         * 
-         * @param member the member to be inserted
-         * @return true iff the member could be inserted. Currently always true
-         */
-        public boolean insertMember(Member member) {
-            members.add(member);
-            return true;
-        }
+		/**
+		 * Inserts a member into the collection
+		 * 
+		 * @param member the member to be inserted
+		 * @return true iff the member could be inserted. Currently always true
+		 */
+		public boolean insertMember(Member member) {
+			members.add(member);
+			return true;
+		}
 
-        public Iterator<Member> iterator() {
-            return members.iterator();
-        }
+		public Iterator<Member> iterator() {
+			return members.iterator();
+		}
 
-        public Iterator<Member> safeIterator() {
-            return new ReadOnlyIterator<Member>(this.members);
-        }
+		public Iterator<Member> safeIterator() {
+			return new ReadOnlyIterator<Member>(this.members);
+		}
 
-        /**
-         * String form of the collection
-         * 
-         */
-        @Override
-        public String toString() {
-            return members.toString();
-        }
-    }
+		/**
+		 * String form of the collection
+		 * 
+		 */
+		@Override
+		public String toString() {
+			return members.toString();
+		}
+	}
 
-    /**
-     * Private for the singleton pattern creates the stock and member collection
-     * objects
-     */
-    private Grocery() {
+	/**
+	 * Private for the singleton pattern creates the stock and member collection
+	 * objects
+	 */
+	private Grocery() {
 
-    }
+	}
 
-    /**
-     * Supports the singleton pattern
-     * 
-     * @return the singleton object
-     */
-    public static Grocery instance() {
-        if (grocery == null) {
-            return grocery = new Grocery();
-        } else {
-            return grocery;
-        }
-    }
+	/**
+	 * Supports the singleton pattern
+	 * 
+	 * @return the singleton object
+	 */
+	public static Grocery instance() {
+		if (grocery == null) {
+			return grocery = new Grocery();
+		} else {
+			return grocery;
+		}
+	}
 
-    /**
-     * 
-     * Member Functions
-     * 
-     */
+	/**
+	 * 
+	 * Member Functions
+	 * 
+	 */
 
-    /**
-     * 
-     * @param request
-     * @return
-     */
-    public Result addMember(Request request) {
-        Result result = new Result();
-        Member member = new Member(request.getMemberName(), request.getMemberAddress(), request.getMemberPhoneNumber(),
-                request.getDateJoined(), request.getFeePaid());
-        if (members.insertMember(member)) {
-            result.setResultCode(Result.OPERATION_COMPLETED);
-            result.setMemberFields(member);
-            return result;
-        }
+	/**
+	 * 
+	 * @param request
+	 * @return
+	 */
+	public Result addMember(Request request) {
+		Result result = new Result();
+		Member member = new Member(request.getMemberName(), request.getMemberAddress(), request.getMemberPhoneNumber(),
+				request.getDateJoined(), request.getFeePaid());
+		if (members.insertMember(member)) {
+			result.setResultCode(Result.OPERATION_COMPLETED);
+			result.setMemberFields(member);
+			return result;
+		}
 
-        result.setResultCode(Result.OPERATION_FAILED);
-        return result;
-    }
+		result.setResultCode(Result.OPERATION_FAILED);
+		return result;
+	}
 
-    public Iterator<Member> getMembers() {
-        return this.members.safeIterator();
-    }
+	public Iterator<Member> getMembers() {
+		return this.members.safeIterator();
+	}
 
-    /**
-     * 
-     * Product Functions
-     * 
-     */
+	/**
+	 * Gets transactions of a member
+	 * 
+	 * @param memberId  memberId of member you want transactions of
+	 * @param startDate start date you want transactions from(inclusive)
+	 * @param endDate   end date of period you want transactions(inclusive)
+	 * @return iterator of transaction items of member requested if memberId and
+	 *         dates are valid
+	 */
+	public Iterator<Transaction> getTransactions(Request request) {
+		Member member = members.search(request.getMemberId());
+		if (member == null || request.getStartDate().after(request.getEndDate())) {
+			return new LinkedList<Transaction>().iterator();
+		}
+		return member.getTransactionsOnDate(request.getStartDate(), request.getEndDate());
+	}
+
+	/**
+	 * 
+	 * Product Functions
+	 * 
+	 */
 }
